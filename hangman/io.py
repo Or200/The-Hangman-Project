f8299a10e7f83fb541fe3d6b4ac38d0575f37ded
Or200 <avimeda74@gmail.com>
2025-10-27 12:43:46 +0200

    add func print status

--- hangman/io.py
+++ hangman/io.py
@@ -5,3 +5,7 @@ def prompt_guess() -> str:
         if char_input.isalpha():
             return char_input
         print("Error - add 1 char in abc")
+
+def print_status(state: dict) -> None:
+    print(f"your word is {"".join(state["display"])}, your guesses is {state["guessed"]}, your Remaining tries is {state["max tries"] - state["wrong_guesses"]}")
+
